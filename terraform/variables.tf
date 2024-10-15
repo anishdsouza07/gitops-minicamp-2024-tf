af0f4403954d4de82ddc9e1fecea9bf341d77109
anishdsouza07 <153701922+anishdsouza07@users.noreply.github.com>
2024-10-15 21:56:04 +0000

    Made some changes to the variables.tf file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,10 @@
-variable "region" {}
-variable "instance_type" {}
+variable "region" {
+description = "The region where the instance is deployed"
+type = string
+default = "us-east-1"
+}
+variable "instance_type" {
+    description = "The instance type"
+    type = string
+    default = "t3.micro"
+}
