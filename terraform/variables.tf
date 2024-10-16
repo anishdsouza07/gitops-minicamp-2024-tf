32730aaf24cd90d3b5c53e18c12898df69b130e0
anishdsouza07 <153701922+anishdsouza07@users.noreply.github.com>
2024-10-16 03:04:01 +0000

    Made some changes with fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,10 @@
 variable "region" {
-description = "The region where the instance is deployed"
-type = string
-default = "us-east-1"
+  description = "The region where the instance is deployed"
+  type        = string
+  default     = "us-east-1"
 }
 variable "instance_type" {
-    description = "The instance type"
-    type = string
-    default = "t3.micro"
+  description = "The instance type"
+  type        = string
+  default     = "t3.micro"
 }
